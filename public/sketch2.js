9d090c13821548ec7857e87fcf2589d3bad81f1c
matteobisotti <matteo.bisotti@studenti.unimi.it>
2023-10-27 09:04:20 +0200

    dvdLogoBouncing

--- public/sketch2.js
+++ public/sketch2.js
@@ -1,40 +1,59 @@
-var ball = {
-    x: 200,
-    y: 300,
-    speedX : 3,
-    speedY : -4
-};
+let x;
+let y;
+
+let xspeed;
+let yspeed;
+
+let dvd;
+
+let r, g, b;
+
+function preload() {
+  dvd = loadImage('dvd_logo.png');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+    createCanvas(windowWidth, windowHeight);
+    x = random(width);
+    y = random(height);
+    xspeed = 4;
+    yspeed = -3;
+    pickColor();
+}
+
+function pickColor() {
+    r = random(100, 256);
+    g = random(100, 256);
+    b = random(100, 256);
 }
 
 function draw() {
   background(0);
-  
-  move();
-  bounce();
-  display();
-}
+  // rect(x, y, 80, 60);
+  // Draw the DVD logo
+  tint(r, g, b);
+  image(dvd, x, y);
 
-function move() {
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-}
+  x = x + xspeed;
+  y = y + yspeed;
 
-function display() {
-    ellipse(ball.x, ball.y, 100, 50);
-}
+  if (x + dvd.width >= width) {
+    xspeed = -xspeed;
+    x = width - dvd.width;
+    pickColor();
+  } else if (x <= 0) {
+    xspeed = -xspeed;
+    x = 0;
+    pickColor();
+  }
 
-function bounce() {
-    if (ball.x > width || ball.x < 0) {
-        ball.speedX *= -1;
-        fill(random(255), random(255), random(255));
-    }
-
-    if (ball.y > height || ball.y < 0) {
-        ball.speedY *= -1;
-        fill(random(255), random(255), random(255));
-    }
+  if (y + dvd.height >= height) {
+    yspeed = -yspeed;
+    y = height - dvd.height;
+    pickColor();
+  } else if (y <= 0) {
+    yspeed = -yspeed;
+    y = 0;
+    pickColor();
+  }
 }
